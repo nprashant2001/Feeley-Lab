54d474e62e716b5cafd363904286a3f0a5eeeca3
nprashant2001 <78383777+nprashant2001@users.noreply.github.com>
2021-08-09 23:00:45 -0700

    Update HistoGraphMaker.py

--- HistoGraphMaker.py
+++ HistoGraphMaker.py
@@ -3,38 +3,36 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def trichrome_plot(data):
-    df = pd.DataFrame(data)
-    df['percent'] = df['blue']/df['area']*100
-    sns.set(style="ticks")
-    sns.boxplot(x='group', y='percent', hue='side', data= df, palette='Set1', width=1).set_title('Fibrosis Index')
-    plt.show()
+#THE PURPOSE OF THIS PROGRAM IS TO TAKE A CSV OF UNANALYZED DATA AND CREATE A BOXPLOT FROM IT
+#PROS: ITS FREE AND FAST, PRISM ISNT
+#CONS: NO SIGNIFICANCE BARS(WORKING ON IT), CSV FILE HAS TO BE FORMATTED AND NAMED A CERTAIN WAY
 
-def oilredo_plot(data):
-    df = pd.DataFrame(data)
-    df['percent'] = df['red']/df['area']*100
-    sns.set(style="ticks")
-    sns.boxplot(x='group', y='percent', hue='side', data= df, palette='Set1', width=1).set_title('Fat Index')
-    plt.show()
+class Plot:
+    def __init__(self, csv, stain, title, x, y, hue, laminin):
+        self.csv = csv
+        self.stain = stain
+        self.title = title
+        self.x = x
+        self.y = y
+        self.hue = hue
+        self.laminin = laminin
 
-def laminin_plot(data):
-    df = pd.DataFrame(data)
-    sns.set(style="ticks")
-    sns.boxplot(x='group', y='area', hue='side', data= df, palette='Set1', width=1).set_title('Fiber Size')
-    plt.show()
+        data = pd.read_csv(self.csv)
+        df = pd.DataFrame(data)
+        if self.laminin == "N":
+            df['percent'] = df[self.stain] / df['area'] * 100
+        sns.set(style="ticks")
+        sns.boxplot(x=self.x, y=self.y, hue=self.hue, data=df, palette='Set1', width=1).set_title(self.title)
+        plt.show()
 
-def ucp1_plot(data):
-    df = pd.DataFrame(data)
-    df['percent'] = df['ucp1']/df['area']*100
-    sns.set(style="ticks")
-    sns.boxplot(x='group', y='percent', hue='side', data= df, palette='Set1', width=1).set_title('UCP1')
-    plt.show()
 
-def cd31_plot(data):
-    df = pd.DataFrame(data)
-    df['percent'] = df['cd31']/df['area']*100
-    sns.set(style="ticks")
-    sns.boxplot(x='group', y='percent', hue='side', data= df, palette='Set1', width=1).set_title('CD31')
-    plt.show()
 
-data = pd.read_csv('')
+
+Plot()
+
+    #components: Plot('csv file path', 'measured pixels', 'chart title', 'x-axis', 'y-axis', 'sub-component', 'is this a laminin stain? Y/N')
+
+    # Trichrome example: Plot('/Users/prashantn/Downloads/Trichrome.csv', 'blue', 'Fibrosis Index', 'group', 'percent', 'side', 'N')
+
+    #ensure you have ALL components or IT WILL NOT WORK
+    
